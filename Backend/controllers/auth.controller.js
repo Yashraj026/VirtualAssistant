29d078c1e5af188a1e7cc208d2633303a801d586
Yash Raj <yashraj9227@gmail.com>
2026-07-25 00:40:24 +0530

    Update auth.controller.js

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -44,8 +44,8 @@ export const signUP = async (req,res) => {
         }
         res.cookie("token",token,{
             httpOnly : true,
-            secure : false,
-            sameSite : "strict",
+            secure : true,
+            sameSite : "none",
             maxAge : 7*24*60*60*1000
         })
         return res.status(201).json(user)
