97994e701c0bb2e064da6ddd3f44c00e71181b26
Yash Raj <yashraj9227@gmail.com>
2026-07-25 00:41:38 +0530

    Update cookie settings for token in auth controller

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -83,8 +83,8 @@ export const logIN = async (req,res) => {
         }
         res.cookie("token",token,{
             httpOnly : true,
-            secure : false,
-            sameSite : "strict",
+            secure : true,
+            sameSite : "none",
             maxAge : 7*24*60*60*1000
         })
 
